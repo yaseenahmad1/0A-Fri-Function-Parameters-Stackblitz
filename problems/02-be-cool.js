8404d3b271588b2bde94f9faa25d1a409fa18c5e
yaseenahmad1 <yahmad09@gmail.com>
2024-09-11 13:49:13 -0700

    problems solved

--- problems/02-be-cool.js
+++ problems/02-be-cool.js
@@ -5,11 +5,13 @@ the first argument to beCool is "Mary", then the function should return
 "Mary is cool!"
 */
 
-// Your code here 
+function beCool (str) {
+    return (str + " is cool!")
+}
 
-// console.log(beCool("Music"));      // "Music is cool!"
-// console.log(beCool("JavaScript")); // "JavaScript is cool!"
-// console.log(beCool("The world"));  // "The world is cool!"
+console.log(beCool("Music"));      // "Music is cool!"
+console.log(beCool("JavaScript")); // "JavaScript is cool!"
+console.log(beCool("The world"));  // "The world is cool!"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = beCool;
